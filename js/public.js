6e6e7c4c7ad66749fff89c7302041326d05679f5
xue <xue@xuedeMacBook-Air.local>
2020-01-08 21:30:31 +0800

    1月8日

--- js/public.js
+++ js/public.js
@@ -18,3 +18,11 @@
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
+
+
+$(document).ready(function(){
+    $('.search .close').on('click',function(){
+        console.log('123')
+        $(this).siblings('input').val('');
+    })
+});
